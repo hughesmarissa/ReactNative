f97c4aa53fd4349d3c75d2514bc2ba687115499b
Marissa Hughes <hughes.marissa331@gmail.com>
2021-11-22 16:39:42 -0800

    Add Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,6 +4,7 @@ import Directory from './DirectoryComponent';
 import About from './AboutComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Reservation from './ReservationComponent';
+import Favorites from './FavoritesComponent';
 import Constants from 'expo-constants';
 import Contact from './ContactComponent';
 import SafeAreaView from 'react-native-safe-area-view';
@@ -168,6 +169,30 @@ const ReservationNavigator = createStackNavigator(
     }
 );
 
+const FavoritesNavigator = createStackNavigator(
+    {
+        Favorites: { screen: Favorites }
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon 
+                name='heart'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
+
+
 const MainNavigator = createDrawerNavigator(
 	{
 	Home: { 
@@ -203,6 +228,20 @@ const MainNavigator = createDrawerNavigator(
             drawerIcon: ({tintColor}) => (
                 <Icon
                     name='tree'
+                    type='font-awesome'
+                    size={24}
+                    color={tintColor}
+                />
+            )
+        }
+    },
+    Favorites : {
+        screen: FavoritesNavigator,
+        navigationOptions: {
+            drawerLabel: 'My Favorites',
+            drawerIcon: ({tintColor}) => (
+                <Icon
+                    name='heart'
                     type='font-awesome'
                     size={24}
                     color={tintColor}
