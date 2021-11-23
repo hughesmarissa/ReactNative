95e2578f27672b2922b4a8de03cbd3b7c52434a8
Marissa Hughes <hughes.marissa331@gmail.com>
2021-11-23 12:24:39 -0800

    Animatable

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Text, ScrollView, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import Loading from './LoadingComponent';
+import Animated from 'react-native-reanimated';
 
 const mapStateToProps = state => {
     return {
@@ -47,6 +49,7 @@ render() {
                 <Mission />
                 <Card
                     title='Community Partners'>
+                        <Loading />
                 </Card>
             </ScrollView>
         );
@@ -54,25 +57,29 @@ render() {
     if (this.props.partners.errMess) {
         return (
             <ScrollView>
-                <Mission />
-                <Card
-                    title='Community Partners'>
-                    <Text>{this.props.partners.errMess}</Text>
-                </Card>
+                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+                </Animatable.View>
             </ScrollView>
         );
     }
 
     return (
 		<ScrollView>
-			<Mission/>
-			<Card title="Community Partners">
-                <FlatList 
-                    data={this.props.partners.partners}
-                    renderItem={renderPartner}
-                    keyExtractor={item => item.id.toString()}
-                />
-			</Card>
+            <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                <Mission/>
+                <Card title="Community Partners">
+                    <FlatList 
+                        data={this.props.partners.partners}
+                        renderItem={renderPartner}
+                        keyExtractor={item => item.id.toString()}
+                    />
+                </Card>
+            </Animatable.View>
         </ScrollView>
     );
   }
